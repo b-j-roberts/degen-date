f34a4815352dab164ff3eda15389a1239a20209e
Timothée Delabrouille <timothee.delabrouille@gmail.com>
2024-12-12 01:36:10 +0100

    use quretyJson

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,7 +1,6 @@
 package routes
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -11,25 +10,14 @@ import (
 )
 
 func getMemeCoins(w http.ResponseWriter, r *http.Request) {
-	coins, error := dbFunctions.PostgresQuery[MemeCoin]("SELECT * FROM public.MemeCoins WHERE launched = TRUE")
+	coins, error := dbFunctions.PostgresQueryJson[MemeCoin]("SELECT * FROM public.MemeCoins WHERE launched = TRUE")
 	if error != nil {
 		fmt.Println(error)
 		routeutils.WriteErrorJson(w, http.StatusInternalServerError, "Failed to get memecoins")
 		return
 	}
 
-	resp, error := json.Marshal(coins)
-	if error != nil {
-		fmt.Println(error)
-		routeutils.WriteErrorJson(w, http.StatusInternalServerError, "Failed to marshal coins")
-		return
-	}
-	_, error = w.Write(resp)
-	if error != nil {
-		fmt.Println(error)
-		routeutils.WriteErrorJson(w, http.StatusInternalServerError, "Failed to write answer coins")
-		return
-	}
+	routeutils.WriteDataJson(w, string(coins))
 }
 
 func InitRoutes() {
