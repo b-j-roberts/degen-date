61a28db1333c288fdbefe5c54276f3cd6f1d5f95
Eli Karotkin <eli.k@starkware.co>
2024-12-12 05:23:15 +0200

    formatting

--- interface/src/state/tokens-store.ts
+++ interface/src/state/tokens-store.ts
@@ -1,7 +1,7 @@
-import "immer";
-import immer from "immer";
-import { StoreState } from "state";
-import { StateCreator } from "zustand";
+import 'immer'
+import immer from 'immer'
+import { StoreState } from 'state'
+import { StateCreator } from 'zustand'
 
 export type Token = {
   name: string;
@@ -22,27 +22,27 @@ type Actions = {
 const initialState: State = {
   tokens: [
     {
-      name: "Starknet Brother",
-      ticker: "BROTHER",
+      name: 'Starknet Brother',
+      ticker: 'BROTHER',
       balance: 120.0,
       conversion: 0.123,
     },
     {
-      name: "Safe Token 100",
-      ticker: "SAFE",
+      name: 'Safe Token 100',
+      ticker: 'SAFE',
       balance: 42000.0,
       conversion: 0.01,
     },
     {
-      name: "CatsCatsCats",
-      ticker: "CATS",
+      name: 'CatsCatsCats',
+      ticker: 'CATS',
       balance: 100000.0,
       conversion: 0.00001,
     },
     {
-      name: "To The Moon",
-      ticker: "TTM",
-      balance: 10000000000000,
+      name: 'To The Moon',
+      ticker:'TTM',
+      balance: 10000000000,
       conversion: 0.0004,
     },
   ],
@@ -52,7 +52,7 @@ export type TokensSlice = State & Actions;
 
 export const createTokensSlice: StateCreator<
   StoreState,
-  [["zustand/immer", never]],
+  [['zustand/immer', never]],
   [],
   TokensSlice
 > = immer((set) => ({
