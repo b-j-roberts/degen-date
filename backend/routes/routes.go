dda3b3962ffacd9844641a9c6911dc57b9c6aebc
Brandon Roberts <brandonjroberts22@gmail.com>
2024-12-12 02:14:16 +0100

    Upload image

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,8 +2,12 @@ package routes
 
 import (
 	"encoding/json"
+	"fmt"
+	"image"
+	"image/png"
 	"math/rand"
 	"net/http"
+	"os"
 
 	dbFunctions "github.com/b-j-roberts/degen-date/backend/internal/db"
 
@@ -88,6 +92,48 @@ func addCoin(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func uploadMemecoinImage(w http.ResponseWriter, r *http.Request) {
+  file, _, err := r.FormFile("image")
+  if err != nil {
+    routeutils.WriteErrorJson(w, http.StatusBadRequest, "Failed to read image")
+    return
+  }
+  defer file.Close()
+
+  r.ParseForm()
+  contractAddress := r.FormValue("contractAddress")
+
+  img, _, err := image.Decode(file)
+  if err != nil {
+    routeutils.WriteErrorJson(w, http.StatusBadRequest, "Failed to decode image")
+    return
+  }
+
+  if _, err := os.Stat("memecoins"); os.IsNotExist(err) {
+    err = os.Mkdir("memecoins", os.ModePerm)
+    if err != nil {
+      routeutils.WriteErrorJson(w, http.StatusInternalServerError, "Failed to create stencils directory")
+      return
+    }
+  }
+
+  filename := fmt.Sprintf("memecoins/%s.png", contractAddress)
+  newimg, err := os.Create(filename)
+  if err != nil {
+    routeutils.WriteErrorJson(w, http.StatusInternalServerError, "Failed to create file")
+    return
+  }
+  defer newimg.Close()
+
+  err = png.Encode(newimg, img)
+  if err != nil {
+    routeutils.WriteErrorJson(w, http.StatusInternalServerError, "Failed to encode image")
+    return
+  }
+
+  routeutils.WriteResultJson(w, "Image uploaded successfully")
+}
+
 func InitRoutes() {
 	// Base route needed for health checks
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
@@ -97,4 +143,6 @@ func InitRoutes() {
 	http.HandleFunc("/coin", addCoin)
 	http.HandleFunc("/coin/{id}", getCoinById)
 	http.HandleFunc("/lineup", getCoinLineup)
+  http.HandleFunc("/upload-memecoin-image", uploadMemecoinImage)
+  http.Handle("/memecoins/", http.StripPrefix("/memecoins/", http.FileServer(http.Dir("./memecoins"))))
 }
