bd5edab3f9493732b05d6cc7cbbf3a7c00938f6b
Eli Karotkin <eli.k@starkware.co>
2024-12-12 05:10:38 +0200

    added state and connection to zustand to dashboard

--- interface/src/state/tokens-store.ts
+++ interface/src/state/tokens-store.ts
@@ -1,13 +1,13 @@
 import "immer";
-import { create } from "zustand";
-import { immer } from "zustand/middleware/immer";
+import immer from "immer";
+import { StoreState } from "state";
+import { StateCreator } from "zustand";
 
 export type Token = {
   name: string;
   ticker: string;
-  imageUrl: string;
-  marketCap: number;
-  holders: number;
+  balance: number;
+  conversion: number;
 };
 
 type State = {
@@ -20,19 +20,46 @@ type Actions = {
 };
 
 const initialState: State = {
-  tokens: [],
+  tokens: [
+    {
+      name: "Starknet Brother",
+      ticker: "BROTHER",
+      balance: 120.0,
+      conversion: 0.123,
+    },
+    {
+      name: "Safe Token 100",
+      ticker: "SAFE",
+      balance: 42000.0,
+      conversion: 0.01,
+    },
+    {
+      name: "CatsCatsCats",
+      ticker: "CATS",
+      balance: 100000.0,
+      conversion: 0.00001,
+    },
+    {
+      name: "To The Moon",
+      ticker: "TTM",
+      balance: 10000000000000,
+      conversion: 0.0004,
+    },
+  ],
 };
 
-export const useBoughtTokensStore = create<
-  State & Actions,
-  [["zustand/immer", never]]
->(
-  immer((set) => ({
-    ...initialState,
-    addToken: (token: Token) =>
-      set((state) => {
-        state.tokens.push(token);
-      }),
-    clearTokens: () => set({ tokens: [] }),
-  }))
-);
+export type TokensSlice = State & Actions;
+
+export const createTokensSlice: StateCreator<
+  StoreState,
+  [["zustand/immer", never]],
+  [],
+  TokensSlice
+> = immer((set) => ({
+  ...initialState,
+  addToken: (token: Token) =>
+    set((state: any) => {
+      state.tokens.push(token);
+    }),
+  clearTokens: () => set({ tokens: [] }),
+}));
